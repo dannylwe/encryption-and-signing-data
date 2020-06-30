de838d07b3ffee29c49390db1170077af0c0814b
Daniel Lwetabe <acehanks@gmail.com>
2020-06-30 21:17:36 +0300

    create and verify digital signiture

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	msg := []byte("verify this message")
+
+	msgHash := sha256.New()
+	// The hash is what we actually sign
+	_, err := msgHash.Write(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	msgHashSum := msgHash.Sum(nil)
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := privateKey.PublicKey
+	signiture, err := rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, msgHashSum, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = rsa.VerifyPSS(&publicKey, crypto.SHA256, msgHashSum, signiture, nil)
+	if err != nil {
+		log.Fatal("could not verify signiture: ", err)
+	}
+	fmt.Println("signiture verified")
 }
