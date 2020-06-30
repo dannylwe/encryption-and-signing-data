8c424befbc355d1d1dc169b42caf9ae019605b8b
Daniel Lwetabe <acehanks@gmail.com>
2020-06-30 20:16:19 +0300

    create encrypt method. Return encrypted in base64

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello world")
+}
+
+func encrypt(message string, key rsa.PublicKey) string {
+	label := []byte("OAEP Encrypted")
+	random := rand.Reader
+	// if label is not needed, nil 
+	cipherText, err := rsa.EncryptOAEP(sha256.New(), random, &key, message, label)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return base64.StdEncoding.EncodeToString(cipherText)
 }
