60b055f9cdef0c438b509183db78b95c030028c9
Daniel Lwetabe <acehanks@gmail.com>
2020-06-30 20:25:51 +0300

    decrypt method

--- main.go
+++ main.go
@@ -9,16 +9,45 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err)
+	}
+	secretMessage := "The ants in france stay mainly on the plants"
+
+	publicKey := privateKey.PublicKey
+
+	encryptedMessage := encrypt(secretMessage, publicKey)
+
+	fmt.Println("Cipher Text:", encryptedMessage)
+
+	decrypt(encryptedMessage, *privateKey)
+
 }
 
 func encrypt(message string, key rsa.PublicKey) string {
 	label := []byte("OAEP Encrypted")
 	random := rand.Reader
 	// if label is not needed, nil 
-	cipherText, err := rsa.EncryptOAEP(sha256.New(), random, &key, message, label)
+	cipherText, err := rsa.EncryptOAEP(sha256.New(), random, &key, []byte(message), label)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return base64.StdEncoding.EncodeToString(cipherText)
+}
+
+func decrypt(cipherText string, privateKey rsa.PrivateKey) string {
+	cipher, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		fmt.Println(err)
+	}
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	plainText, err := rsa.DecryptOAEP(sha256.New(), rng, &privateKey, cipher, label)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Original string: ", string(plainText))
+	return string(plainText)
 }
