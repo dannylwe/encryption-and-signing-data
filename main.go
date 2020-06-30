ba32fb5f3e540173d86fd1d9a9e27717e1400f92
Daniel Lwetabe <acehanks@gmail.com>
2020-06-30 20:25:51 +0300

    decrypt method

--- main.go
+++ main.go
@@ -16,9 +16,25 @@ func encrypt(message string, key rsa.PublicKey) string {
 	label := []byte("OAEP Encrypted")
 	random := rand.Reader
 	// if label is not needed, nil 
-	cipherText, err := rsa.EncryptOAEP(sha256.New(), random, &key, message, label)
+	cipherText, err := rsa.EncryptOAEP(sha256.New(), random, &key, []byte(message), label)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return base64.StdEncoding.EncodeToString(cipherText)
+}
+
+func decrypt(cipherText string, privateKey rsa.PrivateKey) string {
+	cipher, err := base64.RawStdEncoding.DecodeString(cipherText)
+	if err != nil {
+		fmt.Println(err)
+	}
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	plainText, err := rsa.DecryptOAEP(sha256.New(), rng, &privateKey, cipher, label)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Original string: ", string(plainText))
+	return string(plainText)
 }
